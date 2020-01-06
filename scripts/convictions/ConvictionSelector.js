28b37831cc16a6fd82a19b46ae6cb667872184a6
aborgra <audrey.borgra@gmail.com>
2020-01-06 15:58:30 -0600

    refactored double filtering

--- scripts/convictions/ConvictionSelector.js
+++ scripts/convictions/ConvictionSelector.js
@@ -1,25 +1,25 @@
 import { useConvictions } from "./ConvictionDataProvider.js";
 
-// const eventHub = document.querySelector(".container");
+const eventHub = document.querySelector(".container");
 const convictionListContainer = document.querySelector(".filters__crime");
 
 const ConvictionSelect = () => {
   const convictions = useConvictions();
 
-  // eventHub.addEventListener("change", changeEvent => {
-  //   if (changeEvent.target.id ==="crimeSelect") {
-  //     const selectedCrime = changeEvent.target.value;
-  //     console.log(selectedCrime);
+  eventHub.addEventListener("change", changeEvent => {
+    if (changeEvent.target.id ==="crimeSelect") {
+      const selectedCrime = changeEvent.target.value;
+      console.log(selectedCrime);
 
-  //     const message = new CustomEvent("crimeSelected", {
-  //       detail: {
-  //         crime: selectedCrime
-  //       }
-  //     });
+      const message = new CustomEvent("crimeSelected", {
+        detail: {
+          crime: selectedCrime
+        }
+      });
 
-  //     eventHub.dispatchEvent(message);
-  //   }
-  // });
+      eventHub.dispatchEvent(message);
+    }
+  });
   const render = convictionsCollection => {
     convictionListContainer.innerHTML += `
   <select class="crimeDropdown" id="crimeSelect">
@@ -28,8 +28,7 @@ ${convictionsCollection.map(
   crime => `<option id="crimeSelect">${crime}</option>`
 )}
 </select>
-<button class="showWitnesses" id="showWitnesses">Show Witnesses</button>
-      <button class="witness--close" id="witness--close">Show All Criminals</button>`;
+`;
   };
   render(convictions);
 };
--- scripts/filter/Filter.js
+++ scripts/filter/Filter.js
@@ -1,18 +1,32 @@
 const eventHub = document.querySelector(".container")
+let conviction = null
+let arrestingOfficer = null
+
 
 export const FilterComponent = () => {
 return `
 <button id="filterCriminals">Filter</button>
+<button class="witness--close" id="witness--close">Show All Criminals</button>
+<button class="showWitnesses" id="showWitnesses">Show Witnesses</button>
+      
 `
 }
+
+eventHub.addEventListener("crimeSelected", event => {
+  conviction = event.detail.crime
+})
+
+eventHub.addEventListener("officerSelected", event => {
+  arrestingOfficer = event.detail.officer
+})
 
 eventHub.addEventListener("click", clickEvent => {
   if(clickEvent.target.id === "filterCriminals") {
     console.log("filter event sent")
     const filterClicked = new CustomEvent ("filterClicked", {
       detail: {
-        conviction: document.querySelector("#crimeSelect").value,
-        arrestingOfficer: document.querySelector("#officerSelect").value
+        conviction: conviction,
+        arrestingOfficer: arrestingOfficer
       }
     })
     eventHub.dispatchEvent(filterClicked)
